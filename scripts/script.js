cd357966ec599e5d3208eec28ae34d1acf83f8d4
marcos-0215 <149538998+Marcos-0215@users.noreply.github.com>
2024-05-18 23:10:06 -0300

    Single round logic complete

--- scripts/script.js
+++ scripts/script.js
@@ -22,7 +22,8 @@ function getComputerChoice() {
 }
 
 function getHumanChoice() {
-  return prompt("Type your choice:");
+  return prompt(`OPTIONS:
+  Type your choice:`);
 
 }
 
@@ -30,3 +31,77 @@ let humanScore = 0;
 let computerScore = 0;
 
 
+function playRound(humanChoice, computerChoice) {
+    
+  humanChoice = humanChoice.toLowerCase();
+
+  if (humanChoice == 'rock') {
+
+    switch (computerChoice) {
+      case 'rock':
+        console.log('Computer chose ROCK');
+        console.log("It's a DRAW!");
+        break;
+      
+      case 'paper':
+        console.log('Computer chose PAPER');
+        console.log("You LOSE! Paper beats Rock.");
+        computerScore++;
+        break;
+
+      case 'scissors':
+        console.log('Computer chose SCISSORS');
+        console.log("You WIN! Rock beats Scissors.");
+        humanScore++;
+        break;
+    }
+  } else if (humanChoice == 'paper') {
+
+    switch (computerChoice) {
+      case 'paper':
+        console.log('Computer chose PAPER');
+        console.log("It's a DRAW!");
+        break;
+      
+      case 'scissors':
+        console.log('Computer chose SCISSORS');
+        console.log("You LOSE! Scissors beats Paper.");
+        computerScore++;
+        break;
+
+      case 'rock':
+        console.log('Computer chose ROCK');
+        console.log("You WIN! Paper beats Rock.");
+        humanScore++;
+        break;
+    }
+  } else if (humanChoice == 'scissors') {
+
+    switch (computerChoice) {
+      case 'paper':
+        console.log('Computer chose PAPER');
+        console.log("It's a WIN! Scissors beats Paper.");
+        humanScore++;
+        break;
+      
+      case 'scissors':
+        console.log('Computer chose SCISSORS');
+        console.log("It's a DRAW!");
+        break;
+
+      case 'rock':
+        console.log('Computer chose ROCK');
+        console.log("You LOSE! Rock beats Scissors.");
+        computerScore++;
+        break;
+    }
+  } else {
+    console.log('Type a Valid Option');
+
+  }
+}
+
+const pc = getComputerChoice();
+const human = getHumanChoice();
+
+playRound(human, pc);
