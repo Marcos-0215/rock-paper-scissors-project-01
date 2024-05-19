b41619b12733eb41cfb7a332f43fc1d39413f5c6
marcos-0215 <149538998+Marcos-0215@users.noreply.github.com>
2024-05-18 21:38:40 -0300

    Computer and Human moves FUNCTIONS done

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,29 @@
-alert("test");
+console.log("ROCK, PAPERS AND SCISSORS");
+console.log("OPTIONS:");
+console.log("(1) Rock");
+console.log("(2) Paper");
+console.log("(3) Scissors");
+console.log("Type your choice (only number):");
+
+function getComputerChoice() {
+
+  let result;
+  let num = Math.random();
+  console.log(num);
+
+  if (num<=1/3) {
+    result = 'rock';
+  } else if (num<=2/3) {
+    result = 'paper';
+  } else {
+    result = 'scissors';
+  }
+  return result;
+}
+
+function getHumanChoice() {
+  return prompt("Type your choice:");
+
+}
+
+
